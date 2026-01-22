2c22a3b188102679979114cc022d9ddf60acedf9
Amaan <amaankhanp2001@gmail.com>
2026-01-22 09:30:52 +0530

    fixed

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
 var navbar = document.querySelectorAll(".nav>li>a");
-var currentPage = window.location.pathname.split('/').pop().split('.')[0]; // Get the current page name using url of the page
+var currentPage = window.location.pathname.split("/").pop().split(".")[0]; // Get the current page name using url of the page
 
-
-if(currentPage == "index") {
-    var currentPage = "home";   //file name as "index" for github 
+if (currentPage == "index") {
+  var currentPage = "home"; //file name as "index" for github
 }
 
-
 navbar.forEach((nav) => {
-    var targetLink = nav.getAttribute("data-page");
+  var targetLink = nav.getAttribute("data-page");
 
-    if (targetLink === currentPage) { 
-        return;
-    }
+  if (targetLink === currentPage) {
+    return;
+  }
 
-    var targetSvg = document.querySelector("#" + targetLink + ">path");
+  var targetSvg = document.querySelector("#" + targetLink + ">path");
 
-    nav.addEventListener("mouseover", () => {
-        targetSvg.setAttribute("fill", "#000");
-        nav.classList.add("active");
-        nav.classList.remove("text-white");
-    });
+  nav.addEventListener("mouseover", () => {
+    targetSvg.setAttribute("fill", "#000");
+    nav.classList.add("active");
+    nav.classList.remove("text-white");
+  });
 
-    nav.addEventListener("mouseout", () => {
-        targetSvg.setAttribute("fill", "#fff");
-        nav.classList.remove("active");
-        nav.classList.add("text-white");
-    });
+  nav.addEventListener("mouseout", () => {
+    targetSvg.setAttribute("fill", "#fff");
+    nav.classList.remove("active");
+    nav.classList.add("text-white");
+  });
 });
-
 
 const date = new Date().getFullYear();
 document.querySelector("#date").innerHTML = date;
-
-
-
-
-
-
-
-
-
 
 ////////////////////////////////
 
@@ -50,25 +38,19 @@ const likeIcon = document.querySelector(".like>svg.bi-heart");
 const likeFilledIcon = document.querySelector(".like>svg.bi-heart-fill");
 var likeCount = document.querySelector(".likeCount");
 
+likeIcon.addEventListener("click", () => {
+  likeIcon.style.display = "none";
+  likeFilledIcon.style.display = "inline-block";
+  likeCount.innerHTML = parseInt(likeCount.innerHTML) + 1;
+});
 
-likeIcon.addEventListener("click",()=> {
-    likeIcon.style.display = "none";
-    likeFilledIcon.style.display = "inline-block";
-    likeCount.innerHTML = parseInt(likeCount.innerHTML) + 1;
-});                                     
-
-likeFilledIcon.addEventListener("click", ()=> {
-    likeIcon.style.display = "inline-block";
-    likeFilledIcon.style.display = "none";
-    likeCount.innerHTML = parseInt(likeCount.innerHTML) - 1;
+likeFilledIcon.addEventListener("click", () => {
+  likeIcon.style.display = "inline-block";
+  likeFilledIcon.style.display = "none";
+  likeCount.innerHTML = parseInt(likeCount.innerHTML) - 1;
 });
 
 ///////////////////////////////////
-
-
-
-
-
 
 // /////////////////////////
 // COMMENT FUNCTION
@@ -78,28 +60,35 @@ function handleForm(event) {
   var commentUser = document.querySelector("input[name='Name']").value;
   var commentText = document.querySelector("#commentText").value;
   var commentList = document.querySelector(".commentList");
-    
-  var commentCount = document.querySelector(".commentCount") ;
+
+  //for empty username or comment
+  if (commentText.trim().length === 0 || commentUser.trim().length === 0) {
+    return;
+  }
+
+  var commentCount = document.querySelector(".commentCount");
   commentCount.innerHTML = parseInt(commentCount.innerHTML) + 1;
 
-  //for empty username or comment 
-  if(commentText.length === 0 || commentUser.length === 0) {
-    alert("Name and comment field can't be empty");
-    return;               
-  }
-  
   var commentContainer = document.createElement("div");
   var username = document.createElement("h3");
   var text = document.createElement("p");
 
-  commentContainer.classList.add('comment','border-start','border-dark','border-3','rounded-end','shadow','w-100','p-4','my-3','bg-light');
+  commentContainer.classList.add(
+    "comment",
+    "border-start",
+    "border-dark",
+    "border-3",
+    "rounded-end",
+    "shadow",
+    "w-100",
+    "p-4",
+    "my-3",
+    "bg-light",
+  );
   username.innerHTML = commentUser;
-  text.innerHTML = "> "+commentText;
+  text.innerHTML = "> " + commentText;
 
   commentList.appendChild(commentContainer);
   commentContainer.appendChild(username);
   commentContainer.appendChild(text);
-
-    
 }
-  
